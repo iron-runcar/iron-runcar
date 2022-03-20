3fe5af55e149392f43b35841bfb5ee7fc0df1577
Gustavo Borrero <gustavo.borrero.garcia@gmail.com>
2022-03-20 14:11:11 +0100

    feat: footer component and add in App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Footer from "./components/footer/footer";
 import NavBar from "./components/navbar/navbar";
 
 function App() {
   return (
     <div className="App">
+
+      <div className="Navbar">
+        <NavBar />
+      </div>
       
-      <NavBar />
+      <div className="Footer">
+        <Footer />
+      </div>   
       
     </div>
   );
