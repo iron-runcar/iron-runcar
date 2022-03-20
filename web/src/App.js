ec6e3baa8fa9e9c2a7ef4095c03b8257d80282f6
Gustavo Borrero <gustavo.borrero.garcia@gmail.com>
2022-03-20 13:19:09 +0100

    feat: starting web, navbar component

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./components/navbar/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <NavBar />
+      
+    </div>
+  );
+}
+
+export default App;
